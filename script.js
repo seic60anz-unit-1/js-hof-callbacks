3bdac27d48ecdf0bf6735cbd2766b4c13f9e279c
Penelope Chen <pen.c828@gmail.com>
2022-12-09 12:41:12 +1100

    1st incomplete, 2nd complete

---
+++ script.js
@@ -0,0 +1,59 @@
+// Callback Drills 
+// 1
+// const what = function (nani) {
+//     console.log("yello") 
+// }
+// what()
+
+// 2
+// const where = function (doko) {
+//     console.log("hi")
+// }
+// where()
+
+// 3 
+// what(where)
+
+
+// 4 apparently it's not logging the function reference for me
+
+// 5
+
+// Guess the Log
+// 1 ((param, param2), "hi")
+// const foo = (param, param2) => {
+//     param(param2)
+//   }
+  
+//   const bar = (param) => {
+//     console.log(param)
+//   }
+  
+//   foo(bar, 'hi')
+// oh it's just "hi"
+
+// 2 HELLO
+// const foo = (param1, param2) => {
+//     param1(param2)
+//   }
+  
+//   const baz = (param) => {
+//     console.log(param.toUpperCase())
+//   }
+  
+//   foo(baz, 'hello')
+// yay i got this correct!
+
+// 3 hello hi
+// const foo = (param, param2) => {
+//     param(param2, 'hello')
+//   }
+  
+//   const bar = (param, param2) => {
+//     console.log(param2)
+//   }
+  
+//   foo(bar, 'hi')
+// it's just "hello"
+
+// Electric Mixer
