59be3b596c7d2d2be15f36ad783fecbaf35dc108
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-09 12:27:17 +1100

    Guess the Log

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-const logging = function(param){
-    console.log(param)
-}
-const hi = function(){
-    console.log("hi")
-}
-logging(hi)
+// const logging = function(param){
+//     console.log(param)
+// }
+// const hi = function(){
+//     console.log("hi")
+// }
+// logging(hi)
 
-const AlteredLogging = function(param){
-    param()
-}
-AlteredLogging(hi)
+// const AlteredLogging = function(param){
+//     param()
+// }
+// AlteredLogging(hi)
+
+//Guess the Log
+//1. I guess it will log "hi"!
+const foo = (param, param2) => {
+    param(param2)
+  }
+  
+  const bar = (param) => {
+    console.log(param)
+  }
+  
+  foo(bar, 'hi')
+//2. I guess it will log "HELLO"!
+const foo2 = (param1, param2) => {
+    param1(param2)
+  }
+  
+  const baz = (param) => {
+    console.log(param.toUpperCase())
+  }
+  
+  foo2(baz, 'hello')
+
+//3. I guess it will log "hello"!
+const foo3 = (param, param2) => {
+    param(param2, 'hello')
+  }
+  
+  const bar3 = (param, param2) => {
+    console.log(param2)
+  }
+  
+  foo3(bar3, 'hi')
